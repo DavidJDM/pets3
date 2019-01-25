9c979392b695dbd36a1c44ce2e9b0ec9c7803d44
David Kovalevich <kovalevichdavid@yahoo.com>
2019-01-25 16:01:45 -0500

    Error messages and validation

--- index.php
+++ index.php
@@ -54,34 +54,43 @@ $f3->route('GET /', function() {
 });
 
 //Order route
-$f3->route('GET|POST /order',
-    function($f3) {
+$f3->route('GET|POST /order', function($f3) {
         $_SESSION = array();
 
         if(isset($_POST['animal'])) {
 
             $animal = $_POST['animal'];
-            if(validText($animal)) {
+            if(validString($animal)) {
                 $_SESSION['animal'] = $animal;
                 $f3->reroute('/order2');
             } else {
-                $f3->("errors['animal']", "Please enter an animal.");
+                $f3->set("errors['animal']", "Please enter an animal.");
             }
         }
         $template = new Template();
         echo $template->render('views/form1.html');
     });
 
-$f3->route('GET|POST /order2', function() {
-    $_SESSION['animal'] = $_POST['animal'];
+$f3->route('GET|POST /order2', function($f3) {
+
 
     print_r($_SESSION);
+
+    if(isset($_POST['color'])) {
+        $color = $_POST['color'];
+
+        if(validColor($color)) {
+            $_SESSION['color'] = $color;
+            $f3->reroute('/results');
+        } else {
+            $f3->set("errors['color']", "Please enter a color.");
+        }
+    }
     $template = new Template();
     echo $template->render('views/form2.html');
 });
 
-$f3->route('POST /results', function() {
-    $_SESSION['color'] = $_POST['color'];
+$f3->route('GET|POST /results', function() {
     print_r($_SESSION);
     $template = new Template();
     echo $template->render('views/results.html');
--- model/validation-functions.php
+++ model/validation-functions.php
@@ -9,5 +9,9 @@
 
 function validColor($color) {
     global $f3;
-    return in_array($color, $f3-get('color'));
+    return in_array($color, $f3->get('colors'));
+}
+
+function validString($string) {
+    return !empty($string) && ctype_alpha($string);
 }
