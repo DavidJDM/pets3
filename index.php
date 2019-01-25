e281531ddbe624085d986d7172d78ed2d80b3a8e
ttnny <ttnny@users.noreply.github.com>
2019-01-25 13:35:44 -0500

    Update index.php and form2.html

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ require_once ('vendor/autoload.php');
 
 //Create an instance of the Base class
 $f3 = Base::instance();
+$f3->set('colors', array('pink', 'green', 'blue'));
 
 //Turn of Fat-Free error reporting
 $f3->set('DEBUG', 3);
@@ -60,8 +61,8 @@ $f3->route('POST /order2', function() {
     $_SESSION['animal'] = $_POST['animal'];
 
     print_r($_SESSION);
-    $view = new View();
-    echo $view->render('views/form2.html');
+    $template = new Template();
+    echo $template->render('views/form2.html');
 });
 
 $f3->route('POST /results', function() {
